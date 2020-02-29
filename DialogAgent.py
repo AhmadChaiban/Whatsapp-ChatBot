74be74f355c063a35002817f450aabd751c87605
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-29 20:25:00 +0300

    Refactor (Project ID Changed)

--- DialogAgent.py
+++ DialogAgent.py
@@ -1,10 +1,10 @@
 import os
 import dialogflow_v2 as dialogflow
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "nikubuntu-1-5d7d63a5b8ca.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Ahmad-1-5d7d63a5b8ca.json"
 
 dialogflow_session_client = dialogflow.SessionsClient()
-PROJECT_ID = "nikubuntu-1"
+PROJECT_ID = "Ahmad-1"
 
 def detect_intent_from_text(text, session_id, language_code='en'):
     session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
