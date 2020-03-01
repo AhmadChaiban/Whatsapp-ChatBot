51b54325d9fee0fb0dcd20f143f8bd5428c784b0
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-03-01 09:27:04 +0300

    Refactor(DialogAgent)
    Refactored the code for the dialogflow agent. Created a class that is now recognized as the chat agent, and that agent can detect intent from text.

--- DialogAgent.py
+++ DialogAgent.py
@@ -3,17 +3,21 @@ import dialogflow_v2 as dialogflow
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Ahmad-1-5d7d63a5b8ca.json"
 
-dialogflow_session_client = dialogflow.SessionsClient()
-PROJECT_ID = "Ahmad-1"
+class ChatAgent:
+    def __init__(self, projectID):
+        self.dialogflow_session_client = dialogflow.SessionsClient()
+        self.PROJECT_ID = projectID
 
-def detect_intent_from_text(text, session_id, language_code='en'):
-    session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
-    text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.types.QueryInput(text=text_input)
-    response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
-    return response.query_result
+    def detect_intent_from_text(self, text, session_id, language_code='en'):
+        session = self.dialogflow_session_client.session_path(self.PROJECT_ID, session_id)
+        text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = self.dialogflow_session_client.detect_intent(session=session, query_input=query_input)
+        return response.query_result
 
-response = detect_intent_from_text("say joke", 12314)
-response.fulfillment_text
-response.intent.display_name
-response.intent_detection_confidence
+if __name__ == '__main__':
+    chat_agent = ChatAgent('Ahmad-1')
+    response = chat_agent.detect_intent_from_text("say joke", 12314)
+    print(f'Fulfillment Text: {response.fulfillment_text}')
+    print(f'Display Name: {response.intent.display_name}')
+    print(f'Detection Confidence: {response.intent_detection_confidence}')
