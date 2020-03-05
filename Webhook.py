720c2b5dab8635cab86c20c21cb58a6b13749370
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-03-05 11:12:49 +0300

    Feat (Getting response from Bot)
    -- Added the ability to integrate any bot from dialogflow. 
    -- Can now converse with the bot on whatsapp after integration

--- Webhook.py
+++ Webhook.py
@@ -6,13 +6,13 @@ flask_app = Flask(__name__)
 chat_agent = ChatAgent('agent name')
 
 @flask_app.route('/sms', methods = ['POST'])
-def sms_reply(self):
+def smsReply():
     """Respond to incoming calls with a simple text message."""
     # Fetching the message from the chat
     msg = request.form.get('Body')
     # Creating a reply with twilio
     resp = MessagingResponse()
-    resp.message(f"You said: {msg}")
+    resp.message(chat_agent.detect_intent_from_text(msg, 12314).fulfillment_text)
     return str(resp)
 
 flask_app.run(debug = True)
